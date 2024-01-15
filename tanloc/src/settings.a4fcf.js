c7db425ad31eea01228c6d66dc7fba9755c66730
minh quan <>
2024-01-15 15:24:21 +0700

    tl

---
+++ tanloc/src/settings.a4fcf.js
@@ -0,0 +1,53 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Loading/res/scenes/Loading.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/plugins/SAT.2019a.js",
+        "assets/plugins/base64.4755a.js",
+        "assets/plugins/md5.28ad6.js",
+        "assets/plugins/msgpack.7da38.js"
+    ],
+    bundleVers: {
+        internal: "25355",
+        resources: "a5911",
+        bacay: "9f273",
+        baicao: "8c3da",
+        baucua: "edf2d",
+        caothap: "eadde",
+        lieng: "a4157",
+        lode: "aaeb7",
+        maingame: "78c9a",
+        maubinh: "bd0c9",
+        minipoker: "296d5",
+        oantuti: "a60b4",
+        poker: "63e65",
+        pokergo: "9f644",
+        sam: "3f7cc",
+        banca: "29583",
+        caykhe: "fbc74",
+        khobau: "b560c",
+        nudiepvien: "7eef2",
+        sieuanhhung: "cab8a",
+        vuongquocvin: "c74a0",
+        taixiu: "ecebb",
+        taixiumd5: "832da",
+        tienlen: "d17f6",
+        xidzach: "cce13",
+        xocdia: "8f227",
+        main: "2d8a5"
+    }
+};
