8ac6a2d8451035cceae3f83ad44d53c15d8cf5e9
akasa <akasaxlsx@gmail.com>
2024-03-09 21:16:03 +0700

    update

---
+++ slotcaykhe/src/settings.9546f.js
@@ -0,0 +1,54 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Loading/res/scenes/Loading.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/plugins/SAT.2019a.js",
+        "assets/plugins/base64.4755a.js",
+        "assets/plugins/md5.28ad6.js",
+        "assets/plugins/msgpack.7da38.js"
+    ],
+    bundleVers: {
+        internal: "25355",
+        resources: "a5911",
+        bacay: "9f273",
+        baicao: "8c3da",
+        baucua: "88770",
+        caothap: "eadde",
+        lieng: "aacfc",
+        lode: "4dd19",
+        maingame: "2baaa",
+        maubinh: "e7057",
+        minipoker: "296d5",
+        oantuti: "c6c80",
+        poker: "f5716",
+        pokergo: "1d1ce",
+        sam: "fff99",
+        banca: "6ed3b",
+        caykhe: "cb922",
+        khobau: "08900",
+        nudiepvien: "24596",
+        sieuanhhung: "23d4a",
+        vuongquocvin: "a4009",
+        taixiu: "f20ab",
+        taixiumd5: "e4d08",
+        tanloc: "cd3c0",
+        tienlen: "c03fd",
+        xidzach: "cce13",
+        xocdia: "8f227",
+        main: "c85c2"
+    }
+};
