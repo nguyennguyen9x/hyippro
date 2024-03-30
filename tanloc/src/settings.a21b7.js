e35d571b2c961e4679c97f0ec4560e85c9be0372
akasa <akasaxlsx@gmail.com>
2024-03-30 20:19:53 +0700

    tan loc

---
+++ tanloc/src/settings.a21b7.js
@@ -0,0 +1,54 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Loading/res/scenes/Loading.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/plugins/SAT.2019a.js",
+        "assets/plugins/base64.4755a.js",
+        "assets/plugins/md5.28ad6.js",
+        "assets/plugins/msgpack.7da38.js"
+    ],
+    bundleVers: {
+        internal: "25355",
+        resources: "a5911",
+        bacay: "a9e8a",
+        baicao: "8c3da",
+        baucua: "88770",
+        caothap: "eadde",
+        lieng: "59fc2",
+        lode: "177de",
+        maingame: "7f346",
+        maubinh: "e7057",
+        minipoker: "296d5",
+        oantuti: "c6c80",
+        poker: "95880",
+        pokergo: "1d1ce",
+        sam: "f749c",
+        banca: "6ed3b",
+        caykhe: "a6be7",
+        khobau: "5f546",
+        nudiepvien: "34fdc",
+        sieuanhhung: "6d869",
+        vuongquocvin: "e7e7b",
+        taixiu: "f20ab",
+        taixiumd5: "aa24c",
+        tanloc: "a1542",
+        tienlen: "f90e3",
+        xidzach: "76e87",
+        xocdia: "5cdf0",
+        main: "f42b7"
+    }
+};
