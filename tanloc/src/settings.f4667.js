0be8a870934901bb63cee2949902d52664335a44
akasa <akasaxlsx@gmail.com>
2024-04-06 15:57:20 +0700

    remove ping slot

---
+++ tanloc/src/settings.f4667.js
@@ -0,0 +1,54 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Loading/res/scenes/Loading.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/plugins/SAT.2019a.js",
+        "assets/plugins/base64.4755a.js",
+        "assets/plugins/md5.28ad6.js",
+        "assets/plugins/msgpack.7da38.js"
+    ],
+    bundleVers: {
+        internal: "25355",
+        resources: "a5911",
+        bacay: "9f273",
+        baicao: "8c3da",
+        baucua: "88770",
+        caothap: "eadde",
+        lieng: "b0bb5",
+        lode: "cf911",
+        maingame: "190e9",
+        maubinh: "e7057",
+        minipoker: "296d5",
+        oantuti: "c6c80",
+        poker: "95880",
+        pokergo: "1d1ce",
+        sam: "fbc63",
+        banca: "6ed3b",
+        caykhe: "cab06",
+        khobau: "e2199",
+        nudiepvien: "8d6d1",
+        sieuanhhung: "23d4a",
+        vuongquocvin: "a38d5",
+        taixiu: "f20ab",
+        taixiumd5: "36e91",
+        tanloc: "63a33",
+        tienlen: "7f84b",
+        xidzach: "a76e7",
+        xocdia: "5cdf0",
+        main: "a83fd"
+    }
+};
